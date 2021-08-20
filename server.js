3507d5a6686745074ef1782fff387d9b7092ed44
avidrunner87 <36962520+avidrunner87@users.noreply.github.com>
2021-08-19 19:45:08 -0500

    Added Employee functions and menu options

--- server.js
+++ server.js
@@ -16,19 +16,19 @@ function mainMenu() {
             type: 'list',
             name: 'mainMenu',
             message: 'What would you like to do?',
-            choices: ['View All Employees', 'Add/Update Employee', 'View All Roles', 'Add/Update Role', 'View All Departments', 'Add/Update Department', 'Exit']
+            choices: ['View All Employees', 'Add/Update Employee', 'View All Roles', 'Add/Update Role', 'View All Departments', 'Add/Update Department', 'Additional Reports', 'Exit']
         }
     ])
     .then((res) => {
         switch (res.mainMenu) {
             case 'View All Employees':
-                console.log("View Employees");
-                mainMenu();
+                console.log('\n');
+                viewEmployees();
                 break;
             
             case 'Add/Update Employee':
-                console.log("Add/Update Employee");
-                mainMenu();
+                console.log('\n');
+                modifyEmployees();
                 break;
             
             case 'View All Roles':
@@ -49,6 +49,11 @@ function mainMenu() {
             case 'Add/Update Department':
                 console.log('\n');
                 modifyDepartments();
+                break;
+
+            case 'Additional Reports':
+                console.log('\n');
+                additionalReports();
                 break;
 
             default:
@@ -224,4 +229,126 @@ async function deleteRole() {
     }
     
     modifyRoles();
+}
+
+function modifyEmployees() {
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'modifyEmployees',
+            message: 'What would you like to do?',
+            choices: ['Add Employee', 'Edit Employee', 'Delete Employee', "Go Back"]
+        }
+    ])
+    .then((res) => {
+        switch (res.modifyEmployees) {
+            case 'Add Employee':
+                console.log('\n');
+                addEmployee();
+                break;
+            
+            case 'Edit Employee':
+                console.log('\n');
+                editEmployee();
+                break;
+            
+            case 'Delete Employee':
+                console.log('\n');
+                deleteEmployee();
+                break;
+
+            default:
+                console.log('\n');
+                mainMenu();
+        }
+    })
+}
+
+async function viewEmployees() {
+    const employee = new Employee();
+    const viewEmployees = await employee.viewAll();
+
+    mainMenu();
+}
+
+async function addEmployee() {
+    const employee = new Employee();
+    const role = new Role();
+
+    const viewRoles = await role.viewAll();
+    if(viewRoles !== 0) {
+        const newEmployee = await employee.insertNew();
+
+        console.log(`\nSuccessfully added ${newEmployee.first_name} ${newEmployee.last_name}!\n`);
+    } else {
+        console.log('Please define at least one role before trying to create a new employee\n')
+    }
+
+    modifyEmployees();
+}
+
+async function editEmployee() {
+    const employee = new Employee();
+
+    const viewEmployees = await employee.viewAll();
+    if(viewEmployees !== 0) {
+        const editEmployee = await employee.updateExisting();
+
+        console.log(`\nSuccessfully updated the ${editEmployee.employeeField} field!\n`)
+    } else {
+        console.log('Please define at least one employee.\n')
+    }
+
+    modifyEmployees();
+}
+
+async function deleteEmployee() {
+    const employee = new Employee();
+    
+    const viewEmployees = await employee.viewAll();
+    if(viewEmployees !== 0) {
+        const deleteEmployee = await employee.deleteExisting();
+
+    } else {
+        console.log('Please define at least one employee.\n')
+    }
+    
+    modifyEmployees();
+}
+
+function additionalReports() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'report',
+            message: 'What report would you like to see?',
+            choices: ['Employees by Manager', 'Employees by Department', 'Budget by Department', "Go Back"]
+        }
+    ])
+    .then((res) => {
+        switch (res.modifyEmployees) {
+            case 'Employees by Manager':
+                console.log('\n');
+                const employee = new Employee();
+                const viewEmployees = await employee.byManager();
+                break;
+            
+            case 'Employees by Department':
+                console.log('\n');
+                const employee = new Employee();
+                const viewEmployees = await employee.byDepartment();
+                break;
+            
+            case 'Budget by Department':
+                console.log('\n');
+                const department = new Department();
+                const viewDepartments = await department.budget();
+                break;
+
+            default:
+                console.log('\n');
+                mainMenu();
+        }
+    })
 }
